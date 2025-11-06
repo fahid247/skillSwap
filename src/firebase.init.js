cd447de0a5b121f003bb0cb24f6e88c19443f6e0
Fahid Aqundow <136666497+fahid247@users.noreply.github.com>
2025-11-06 10:49:07 +0600

    done with authentication only eye button is left

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_hsAhsniIKEUrjsHR6QL7hB14neflKRc",
-  authDomain: "skillswap-de43c.firebaseapp.com",
-  projectId: "skillswap-de43c",
-  storageBucket: "skillswap-de43c.firebasestorage.app",
-  messagingSenderId: "1018242953837",
-  appId: "1:1018242953837:web:650fa47d7fb9e974b68564"
+  apiKey: import.meta.env.VITE_apiKey, 
+  authDomain: import.meta.env.VITE_authDomain, 
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket, 
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId, 
 };
 
 // Initialize Firebase
